9427855536611ee4f3876056c25261cf36a9a340
Aqz <Kzak.Gluhov@gmail.com>
2022-05-07 16:04:27 +0400

    v2.1

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -2,7 +2,7 @@ package ru.netology.manager;
 
 import ru.netology.domain.Post;
 
-public class PostManager extends AllManager {
+public class PostManager  {
     private Post[] posts;
     public String generatePost(){
         //to do;
